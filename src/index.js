ffb05236f8b0cce36215cd6eaa751480fcbf046d
stimulk08 <63906786+stimulk08@users.noreply.github.com>
2023-04-14 20:47:05 +0500

    initial commit & bics parse

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import Zip from 'adm-zip';
+import fetch from 'node-fetch';
+import iconLite from 'iconv-lite';
+import fs from 'fs';
+import { xml2js } from 'xml-js';
+
+class BicsParser {
+    bikSourceUrl;
+    constructor(bikSourceUrl) {
+        this.bikSourceUrl = bikSourceUrl;
+    }
+
+    fetchBicsArchive() {
+        return new Promise(async (resolve, reject) => {
+            const res = await fetch(this.bikSourceUrl);
+
+            if (!res.ok)
+                return reject(new Error(`Fetch failed with status code ${res.status}`));
+            
+            const arrayBuffer = await res.arrayBuffer();
+
+            return resolve(Buffer.from(arrayBuffer));
+        })
+    }
+
+    parseByJson(bicXml) {
+        const bicJsons = xml2js(bicXml).elements[0].elements;
+        return bicJsons.reduce((bics, bik) => {
+            const bicInfos = bik.elements;
+            if (!bicInfos) return bics;
+
+            const accountIds = [];
+            let name;
+
+            bicInfos.forEach(info => {
+                if (info.name === 'Accounts' && info.attributes.Account) {
+                    accountIds.push(info.attributes.Account)
+                }
+                else if (info.name === 'ParticipantInfo') {
+                    name = info.attributes.NameP;
+                }
+            });
+
+            accountIds.forEach(corrAccount => bics.push({ 
+                bik: bik.attributes.BIC,
+                name,
+                corrAccount 
+            }));
+
+            return bics;
+        }, []);
+    }
+
+    async getBics() {
+        return new Promise((resolve, reject) => {
+            return this.fetchBicsArchive()
+                .then(archiveBuffer => {
+                    const xmlEntry = new Zip(archiveBuffer).getEntries()[0];
+                    xmlEntry.getDataAsync(data => {
+                        const bikXml = iconLite.decode(data, 'windows-1251');
+                        return resolve(this.parseByJson(bikXml));
+                });
+            });
+        })
+    }
+}
+
+(async () => {
+    const parser = new BicsParser('http://www.cbr.ru/s/newbik1');
+    const bics = await parser.getBics().catch(console.log);
+    console.log(bics);
+})();
+
+
+
